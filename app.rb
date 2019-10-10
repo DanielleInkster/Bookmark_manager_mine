3e04847abfd7cf3d002868902a94f1ab3aa1ffdb
DanielleInkster <danielle.inkster@gmail.com>
2019-10-10 22:58:51 +0100

    Able to edit links

--- app.rb
+++ app.rb
@@ -26,4 +26,15 @@ class BookmarkManager < Sinatra::Base
     Bookmark.delete(id: params[:id])
     redirect '/bookmarks'
   end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :"bookmarks/edits"
+  end
+
 end
